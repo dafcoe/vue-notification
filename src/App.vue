4fbda101d58d3baaca88304d1386c8529bcd2f16
daniel.coelho <daniel.coelho@turbinekreuzberg.com>
2021-03-24 00:48:13 +0000

    v0.1.1

--- src/App.vue
+++ src/App.vue
@@ -1,164 +1,498 @@
 <template>
-  <div class="wrapper">
-    <vue-swappable-card>
-      <template #content-primary>
-        <h1>Front Face A</h1>
-        <div class="card-body">
-          <img src="https://picsum.photos/seed/abc1/100">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec malesuada ipsum. Morbi sit amet sagittis justo. Praesent non est eu lorem rhoncus scelerisque eget non nisi.</p>
-          <p>Nulla sem diam, sollicitudin non metus vel, eleifend congue neque.</p>
-        </div>
-      </template>
-      <template #content-secondary>
-        <h1>Back Face A</h1>
-        <div class="card-body">
-          <img src="https://picsum.photos/seed/cba/100">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec malesuada ipsum. Morbi sit amet sagittis justo. Praesent non est eu lorem rhoncus scelerisque eget non nisi.</p>
-          <p>Nulla sem diam, sollicitudin non metus vel, eleifend congue neque.</p>
-        </div>
-      </template>
-    </vue-swappable-card>
+  <div>
+    <h1>@dafcoe/vue-notification</h1>
 
-    <vue-swappable-card direction="from-top">
-      <template #content-primary>
-        <h1>Front Face B</h1>
-        <div class="card-body">
-          <img src="https://picsum.photos/seed/abc1-1/100">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec malesuada ipsum. Morbi sit amet sagittis justo. Praesent non est eu lorem rhoncus scelerisque eget non nisi.</p>
-          <p>Nulla sem diam, sollicitudin non metus vel, eleifend congue neque.</p>
+    <div class="wrapper">
+      <div class="dashboard--left">
+        <div class="dashboard">
+          <div class="dashboard__row">
+            <span class="dashboard__row-label">Position:</span>
+            <span class="dashboard__row-content">
+              <span
+                v-for="notificationPosition in notificationPositions"
+                :key="notificationPosition"
+              >
+                <input
+                  v-model="position"
+                  :id="notificationPosition"
+                  :value="notificationPosition"
+                  type="radio"
+                >
+                <label :for="notificationPosition">
+                  {{ notificationPosition }}
+                </label>
+              </span>
+            </span>
+          </div>
         </div>
-      </template>
-      <template #content-secondary>
-        <h1>Back Face B</h1>
-        <div class="card-body">
-          <img src="https://picsum.photos/seed/cba-1/100">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec malesuada ipsum. Morbi sit amet sagittis justo. Praesent non est eu lorem rhoncus scelerisque eget non nisi.</p>
-          <p>Nulla sem diam, sollicitudin non metus vel, eleifend congue neque.</p>
-        </div>
-      </template>
-    </vue-swappable-card>
 
-    <vue-swappable-card direction="from-left">
-      <template #content-primary>
-        <h1>Front Face C</h1>
-        <div class="card-body">
-          <img src="https://picsum.photos/seed/abc1-2/100">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec malesuada ipsum. Morbi sit amet sagittis justo. Praesent non est eu lorem rhoncus scelerisque eget non nisi.</p>
-          <p>Nulla sem diam, sollicitudin non metus vel, eleifend congue neque.</p>
-        </div>
-      </template>
-      <template #content-secondary>
-        <h1>Back Face C</h1>
-        <div class="card-body">
-          <img src="https://picsum.photos/seed/cba-2/100">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec malesuada ipsum. Morbi sit amet sagittis justo. Praesent non est eu lorem rhoncus scelerisque eget non nisi.</p>
-          <p>Nulla sem diam, sollicitudin non metus vel, eleifend congue neque.</p>
-        </div>
-      </template>
-    </vue-swappable-card>
+        <div class="dashboard">
+          <div class="dashboard__row">
+            <span class="dashboard__row-content">
+              <input
+                v-model.trim="message"
+                type="text"
+                placeholder="message"
+              >
+            </span>
+          </div>
 
-    <vue-swappable-card direction="from-right">
-      <template #content-primary>
-        <h1>Front Face D</h1>
-        <div class="card-body">
-          <img src="https://picsum.photos/seed/abc1-3/100">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec malesuada ipsum. Morbi sit amet sagittis justo. Praesent non est eu lorem rhoncus scelerisque eget non nisi.</p>
-          <p>Nulla sem diam, sollicitudin non metus vel, eleifend congue neque.</p>
+          <div class="dashboard__row">
+            <span class="dashboard__row-label">Type:</span>
+            <span class="dashboard__row-content">
+              <span
+                v-for="notificationType in notificationTypes"
+                :key="notificationType"
+              >
+                <input
+                  v-model="type"
+                  :id="notificationType"
+                  :value="notificationType"
+                  type="radio"
+                >
+                <label :for="notificationType">
+                  {{ notificationType }}
+                </label>
+              </span>
+            </span>
+            <span class="dashboard__row-tip">
+              <input
+                v-model="showIcon"
+                id="showIcon"
+                type="checkbox"
+              >
+              <label for="showIcon">
+                show icon
+              </label>
+            </span>
+          </div>
+
+          <div class="dashboard__row">
+            <span class="dashboard__row-label">Appearance:</span>
+            <span
+              v-for="notificationAppearance in notificationAppearances"
+              :key="notificationAppearance"
+              class="padded-right"
+            >
+              <input
+                v-model="appearance"
+                :id="notificationAppearance"
+                :value="notificationAppearance"
+                type="radio"
+              >
+              <label :for="notificationAppearance">
+                {{ notificationAppearance }}
+              </label>
+            </span>
+          </div>
+
+          <div class="dashboard__row">
+            <span class="dashboard__row-label">Dismiss:</span>
+            <span
+              v-for="dismissMode in ['manually', 'automatically']"
+              :key="dismissMode"
+              class="padded-right"
+            >
+              <input
+                v-model="dismiss[dismissMode]"
+                :id="`dismiss-${dismissMode}`"
+                type="checkbox"
+              >
+              <label :for="`dismiss-${dismissMode}`">
+                {{ dismissMode }}
+              </label>
+            </span>
+          </div>
+
+          <div class="dashboard__row">
+            <span class="dashboard__row-label">Duration:</span>
+            <input
+              v-model.number="duration"
+              :disabled="!dismiss['automatically']"
+              type="number"
+              class="padded-right"
+            >
+            <span>
+              <input
+                v-model="showDurationProgress"
+                :disabled="!dismiss['automatically']"
+                id="showDurationProgress"
+                type="checkbox"
+              >
+              <label for="showDurationProgress">
+                show progress animation
+              </label>
+            </span>
+          </div>
+
+          <div class="dashboard__row">
+            <button @click="pushNotification">
+              Push Notification
+            </button>
+          </div>
         </div>
-      </template>
-      <template #content-secondary>
-        <h1>Back Face D</h1>
-        <div class="card-body">
-          <img src="https://picsum.photos/seed/cba-3/100">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec malesuada ipsum. Morbi sit amet sagittis justo. Praesent non est eu lorem rhoncus scelerisque eget non nisi.</p>
-          <p>Nulla sem diam, sollicitudin non metus vel, eleifend congue neque.</p>
+      </div>
+
+      <div class="dashboard--right">
+        <div class="dashboard">
+          <div class="dashboard__row">
+            <span class="dashboard__row-label">Preview:</span>
+            <div>
+              <vue-notification-list-item
+                :notification="notificationPreview"
+                :layout="notificationLayoutPreview"
+                class="notification-preview-item"
+              />
+            </div>
+          </div>
+          <div class="dashboard__row">
+            <pre>{{ notificationPreview }}</pre>
+          </div>
         </div>
-      </template>
-    </vue-swappable-card>
+      </div>
+    </div>
+
+    <vue-notification-list :position="position" />
+
+    <div class="bottom-links">
+      <a
+        href="https://github.com/dafcoe/vue-notification"
+        target="_blank"
+      >
+        <span
+          v-html="icons.github"
+          class="bottom-link--github"
+        />
+      </a>
+      <a
+        href="https://www.npmjs.com/package/@dafcoe/vue-notification"
+        target="_blank"
+      >
+        <span
+          v-html="icons.npm"
+          class="bottom-link--npm"
+        />
+      </a>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import VueSwappableCard from '@/components/VueSwappableCard.vue'
+import {
+  computed,
+  defineComponent,
+  reactive,
+  ref,
+  toRefs,
+} from 'vue'
+import VueNotificationList from '@/components/VueNotificationList.vue'
+import VueNotificationListItem from '@/components/VueNotificationListItem.vue'
+import { Notification } from '@/components/vue-notification.interface'
+import {
+  NotificationListItemAppearance,
+  NotificationListItemType,
+  NotificationListPosition,
+} from '@/components/vue-notification.enum'
+import { useNotificationStore } from '@/components/vue-notification.store'
 
 export default defineComponent({
   name: 'App',
   components: {
-    VueSwappableCard,
+    VueNotificationList,
+    VueNotificationListItem,
+  },
+  setup() {
+    const notification = reactive<Partial<Notification>>({
+      message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      type: 'info' as NotificationListItemType,
+      showIcon: true,
+      dismiss: {
+        manually: true,
+        automatically: true,
+      },
+      duration: 10000,
+      showDurationProgress: true,
+      appearance: 'light' as NotificationListItemAppearance,
+    })
+
+    const notificationPreview = ref(notification)
+
+    const position = ref(NotificationListPosition.BottomRight)
+
+    const notificationLayoutPreview = computed(
+      () => position.value.replace('top-', '').replace('bottom-', ''),
+    )
+
+    const notificationTypes = ref(Object.values(NotificationListItemType))
+
+    const notificationAppearances = ref(Object.values(NotificationListItemAppearance))
+
+    const notificationPositions = ref(Object.values(NotificationListPosition))
+
+    const icons = {
+      github: `
+        <svg width="24px" height="24px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
+            <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/>
+        </svg>
+      `,
+      npm: `
+        <svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
+          <g>
+              <rect width="24" height="24" opacity="0"/>
+              <path d="M18 3H6a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h7V11h4v10h1a3 3 0 0 0 3-3V6a3 3 0 0 0-3-3z"/>
+          </g>
+        </svg>
+      `,
+    }
+
+    const { setNotification } = useNotificationStore()
+
+    const pushNotification = (): void => {
+      setNotification(notification)
+    }
+
+    return {
+      ...toRefs(notification),
+      notificationPreview,
+      position,
+      notificationLayoutPreview,
+      notificationTypes,
+      notificationAppearances,
+      notificationPositions,
+      icons,
+      pushNotification,
+    }
   },
 })
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
   @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;500&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap');
 
-  .wrapper {
+  body {
     display: flex;
     justify-content: center;
-    align-items: flex-start;
-    margin: 0 auto;
-    max-width: 1700px;
+    align-items: center;
     height: 100vh;
-  }
-
-  .vsc {
+    width: 100%;
     font-family: 'Roboto', sans-serif;
     font-weight: 300;
     line-height: 1.5;
-    max-width: 380px;
-    margin: 180px auto;
     color: #333;
+    background: url("https://www.transparenttextures.com/patterns/dark-mosaic.png"), linear-gradient(to right, #C6426E, #642B73);
+  }
+
+  * {
     box-sizing: border-box;
+  }
 
-    ::v-deep(.vsc__content) {
-      &--primary {
-        h1 {
-          background-color: darkcyan;
-          color: white;
+  h1 {
+    margin-top: -40px;
+    text-align: center;
+    color: white;
+    font-weight: 300;
+    font-size: 64px;
+  }
+
+  .wrapper {
+    display: flex;
+  }
+
+  .dashboard {
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+    width: 100%;
+    max-width: 545px;
+    background-color: rgba(255, 255, 255, .1);
+    border-radius: 10px;
+    backdrop-filter: blur(5px);
+    border: 1px solid rgba(255, 255, 255, .125);
+    border-top-color: rgba(255, 255, 255, .25);
+    border-left-color: rgba(255, 255, 255, .25);
+    color: #eee;
+
+    &:not(:last-of-type) {
+      margin-bottom: 20px;
+    }
+
+    &--left,
+    &--right {
+      margin: 10px;
+    }
+
+    &--right {
+      .dashboard {
+        height: 100%;
+
+        &__row {
+          overflow: hidden;
         }
       }
-
-      &--secondary {
-        h1 {
-          color: #675000;
-        }
-
-        &::before {
-          content: '';
-          position: absolute;
-          top: 0;
-          bottom: 0;
-          left: 0;
-          z-index: -1;
-          background-color: #f4cb36;
-          width: 88px;
-        }
-      }
     }
 
-    h1 {
-      margin: 0;
-      padding: 12px;
-    }
-
-    img {
-      float: right;
-      border-radius: 4px;
-      border: 1px solid #eee;
-    }
-
-    p {
-      margin: 0;
-      padding: 0;
+    &__row {
+      display: flex;
+      align-items: center;
 
       &:not(:last-of-type) {
-        margin-bottom: 12px;
+        margin-bottom: 20px;
+        padding-bottom: 20px;
+        border-bottom: 1px solid rgba(255, 255, 255, .07);
       }
     }
 
-    .card-body {
+    &__row-label {
+      margin-right: 20px;
+      font-weight: bold;
+    }
+
+    &__row-content {
+      display: flex;
+      justify-content: space-between;
+      flex: 1;
+    }
+
+    &__row-tip {
+      margin-left: 20px;
+      padding-left: 20px;
+      border-left: 1px solid rgba(255, 255, 255, .07);
+    }
+
+    .padded-right {
+      margin-right: 50px;
+    }
+
+    input[type=text] {
       padding: 12px;
+      width: 100%;
+      font-size: 18px;
+      text-align: center;
+      background-color: rgba(255, 255, 255, .25);
+      border: 1px solid rgba(255, 255, 255, .125);
+      border-radius: 5px;
+      color: white;
+      outline: none;
+    }
+
+    input[type=number] {
+      padding: 12px;
+      text-align: center;
+      background-color: rgba(255, 255, 255, .25);
+      border: 1px solid rgba(255, 255, 255, .125);
+      border-radius: 5px;
+      color: white;
+      outline: none;
+    }
+
+    input[type=radio],
+    input[type=checkbox] {
+      position: absolute;
+      left: -100vw;
+
+      &:checked {
+        ~ label {
+          border-color: rgba(255, 255, 255, .75);
+        }
+      }
+
+      &:disabled {
+        ~ label {
+          opacity: .25;
+        }
+      }
+    }
+
+    label {
+      padding: 8px;
+      border: 1px solid transparent;
+      border-radius: 5px;
+      cursor: pointer;
+      transition: all .25s ease-in;
+
+      &:hover {
+        border-color: rgba(255, 255, 255, .25);
+      }
+    }
+
+    input {
+      &[disabled] {
+        transition: all .25s ease-in;
+        opacity: .25;
+      }
+    }
+
+    button {
+      padding: 12px;
+      width: 100%;
+      font-size: 18px;
+      text-align: center;
+      background-color: rgba(255, 255, 255, .75);
+      border: none;
+      border-radius: 5px;
+      outline: none;
+      cursor: pointer;
+      color: #333;
+      transition: all .25s ease-in;
+
+      &:hover {
+        background-color: white;
+        color: black;
+      }
+    }
+
+    .notification-preview-item {
+      margin: 0;
+    }
+
+    pre {
+      margin: 0;
+      font-size: 14px;
+      font-family: 'Source Code Pro', monospace;
+    }
+  }
+
+  .bottom-links {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    a {
+      display: inline-block;
+      margin: 0 10px;
+      color: white;
+      text-decoration: none;
+      opacity: .75;
+      transition: opacity .25s ease-in-out;
+
+      &:hover {
+        opacity: 1;
+      }
+
+      span {
+        display: inline-flex;
+        height: 42px;
+        width: 42px;
+
+        &.bottom-link--github {
+          margin-top: -5px;
+          padding: 5px;
+        }
+      }
+
+      svg {
+        height: 100%;
+        width: 100%;
+      }
+    }
+
+    :visited {
+      color: white;
     }
   }
 </style>
---
+++ src/components/VueNotificationListItem.vue
@@ -0,0 +1,259 @@
+<template>
+  <li
+    :class="classList"
+    class="notification-list-item"
+    @click="dismissManually"
+  >
+    <div
+      ref="tagRef"
+      :style="animationDurationStyle"
+      class="notification-list-item__tag"
+    />
+    <div class="notification-list-item__message">
+      <slot>{{ notification.message }}</slot>
+    </div>
+    <div class="notification-list-item__icon">
+      <span
+        v-if="notification.showIcon"
+        v-html="icons[notification.type]"
+      />
+    </div>
+  </li>
+</template>
+
+<script lang="ts">
+import {
+  computed,
+  defineComponent,
+  onMounted,
+  PropType,
+  ref,
+} from 'vue'
+import { Notification } from '@/components/vue-notification.interface'
+import {
+  NotificationListItemAppearance,
+  NotificationListItemLayout,
+  NotificationListItemType,
+} from '@/components/vue-notification.enum'
+import { useNotificationStore } from '@/components/vue-notification.store'
+import { ICONS } from '@/components/vue-notification.constant'
+
+export default defineComponent({
+  name: 'NotificationListItem',
+  props: {
+    notification: {
+      type: Object as PropType<Notification>,
+      required: true,
+    },
+    layout: {
+      type: String as PropType<NotificationListItemLayout>,
+      default: NotificationListItemLayout.Left,
+    },
+  },
+  setup(props) {
+    const tagRef = ref<HTMLElement>()
+
+    const { unsetNotification } = useNotificationStore()
+
+    const notificationListItemType = computed(
+      () => Object.values(NotificationListItemType).includes(props.notification.type)
+        ? props.notification.type
+        : NotificationListItemType.Info,
+    )
+
+    const notificationListItemLayout = computed(
+      () => Object.values(NotificationListItemLayout).includes(props.layout)
+        ? props.layout
+        : NotificationListItemLayout.Left,
+    )
+
+    const notificationListItemAppearance = computed(
+      () => Object.values(NotificationListItemAppearance).includes(props.notification.appearance)
+        ? props.notification.appearance
+        : NotificationListItemAppearance.Light,
+    )
+
+    const classList = computed(() => ({
+      'notification-list-item--dismissible-manual': props.notification.dismiss.manually,
+      'notification-list-item--dismissible-automatic': props.notification.dismiss.automatically && props.notification.showDurationProgress,
+      [`notification-list-item--${notificationListItemType.value}`]: true,
+      [`notification-list-item--${notificationListItemLayout.value}`]: true,
+      [`notification-list-item--${notificationListItemAppearance.value}`]: true,
+    }))
+
+    const animationDurationStyle = ref({
+      // duration - appear time - start delay - end delay
+      animationDuration: `${props.notification.duration - 500 - 100}ms`,
+    })
+
+    const dismissManually = () => {
+      if (!props.notification.dismiss.manually) return
+
+      unsetNotification(props.notification.id)
+    }
+
+    const dismissAutomatically = () => {
+      if (!props.notification.dismiss.automatically) return
+
+      setTimeout(() => {
+        unsetNotification(props.notification.id)
+      }, props.notification.duration)
+    }
+
+    onMounted(() => {
+      dismissAutomatically()
+    })
+
+    return {
+      tagRef,
+      classList,
+      animationDurationStyle,
+      icons: ICONS,
+      dismissManually,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+  $color-success: #009800;
+  $color-alert: #ff3600;
+  $color-warning: #ffa600;
+  $color-info: #0088ff;
+
+  .notification-list-item {
+    $root: &;
+
+    position: relative;
+    display: flex;
+    margin: 8px;
+    min-height: 74px;
+    width: 100vw;
+    max-width: 400px;
+    border-radius: 4px;
+    overflow: hidden;
+
+    &--light {
+      background-color: #fcfcfc;
+      border: 1px solid #eee;
+      color: #333;
+    }
+
+    &--dark {
+      background-color: #333;
+      border: 1px solid #222;
+      color: #eee;
+    }
+
+    &--glass {
+      background-color: rgba(255, 255, 255, .1);
+      backdrop-filter: blur(5px);
+      border: 1px solid rgba(255, 255, 255, .125);
+      border-top-color: rgba(255, 255, 255, .25);
+      border-left-color: rgba(255, 255, 255, .25);
+      color: #eee;
+    }
+
+    &--dismissible-manual {
+      cursor: pointer;
+    }
+
+    &__tag {
+      position: absolute;
+      bottom: 0;
+      height: 100%;
+      width: 6px;
+
+      #{$root}--dismissible-automatic & {
+        animation-name: progress;
+        animation-timing-function: linear;
+        animation-delay: .5s;
+        animation-fill-mode: forwards;
+      }
+    }
+
+    &__message {
+      z-index: 1;
+      flex: 1;
+      padding: 12px 12px 12px 18px;
+    }
+
+    &__icon {
+      position: absolute;
+      bottom: -24px;
+      opacity: .25;
+      width: 80px;
+
+      #{$root}--glass & {
+        opacity: .5;
+      }
+    }
+
+    &--left {
+      text-align: right;
+
+      #{$root}__tag {
+        right: 0;
+      }
+
+      #{$root}__icon {
+        left: -24px;
+        transform: rotate(12deg);
+      }
+    }
+
+    &--right {
+      #{$root}__icon {
+        right: -24px;
+        transform: rotate(-12deg);
+      }
+    }
+
+    &--success {
+      #{$root}__tag {
+        background-color: $color-success;
+      }
+
+      #{$root}__icon {
+        color: $color-success;
+      }
+    }
+
+    &--alert {
+      #{$root}__tag {
+        background-color: $color-alert;
+      }
+
+      #{$root}__icon {
+        color: $color-alert;
+      }
+    }
+
+    &--warning {
+      #{$root}__tag {
+        background-color: $color-warning;
+      }
+
+      #{$root}__icon {
+        color: $color-warning;
+      }
+    }
+
+    &--info {
+      #{$root}__tag {
+        background-color: $color-info;
+      }
+
+      #{$root}__icon {
+        color: $color-info;
+      }
+    }
+  }
+</style>
+
+<style lang="scss">
+  @keyframes progress {
+    0% { height: 100%; opacity: 1; }
+    100% { height: 0; opacity: .5; }
+  }
+</style>
